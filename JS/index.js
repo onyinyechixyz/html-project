c530d2029368106f0b1a7f5738da8b8181a989e8
onyinyechixyz <nwabuezeprecious66@gmail.com>
2024-08-12 05:19:15 -0700

    first commit

---
+++ JS/index.js
@@ -0,0 +1,79 @@
+//evaluates the data inside of it and prints it to th terminal or console for the developer.
+// console.log(5 + 7);
+
+// alert('hello everyone') //prints a message to the user on  the browser.
+// prompt('how old are you')// collect information from the usersb and evaluate it.
+// confirm('are yousure you want to pay $100?')// ascertain the user's actions on the webpage.
+
+// var
+// let //let will allow you reassign a value to a variable after you have declared it.
+// const //let will not allow you  reassign a value to a variable after you have declared it.
+
+// let car= 'porshe'
+// car='BMW'
+// console.log(car);
+
+//  data types
+// primitive data types 
+// number data types // they are variables that has been assigned a numeric value
+// let age= 30 
+// console.log (age);
+
+// //string data types // they are variables that have been assigned textual values and are placed inside a quote
+// let name ='luca' 
+// console.log(name);
+
+// //boolean data types // boolean variables only have two  options: true or false
+// let male =true 
+// console.log(male);
+
+// //null data types  // they are variables that has been assigned but the value is nothing.
+//  let account = null 
+// console.log(account);
+
+// // undefined data types // they are variables that have been declared but not assigned a value
+//  let date; 
+// console.log(date);
+
+//non primitive data types
+// functions // are blocks of code used to evaluate logic and return a value
+// arguments are the values of parameters
+// function add (a, b){
+    // console.log(a * b);
+// }
+// add(10, 5)
+
+//date data types // are used to display the current date in javascript
+//const time =new date().getday()
+// console.log(time);
+
+// array data types // are used to store multiple data and their datatypes in the same variable
+// length calculates the total number of items in the array 
+// index calculates the position of items in the array
+// const items = [1,'john', null, true, 'bmw']
+// console.log(items, length);
+// console.log(items[3]);
+
+// const colours =['black', 'white', 'red', 'green']
+// colours.unshift('blue', 'indigo')
+// console.log(colours);
+
+//  const colours =['black', 'white', 'red', 'green']
+//  colours.shift()
+//  console.log(colours);
+
+// const colours =['black', 'white', 'red', 'green']
+// colours.push('blue', 'indigo')
+// console.log(colours);
+
+const colours =['black', 'white', 'red', 'green']
+colours.pop()
+console.log(colours);
+
+
+
+ 
+
+
+
+
